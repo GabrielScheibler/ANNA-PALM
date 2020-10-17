fixed weight splitting error in revnet generation removed tf.stop_gradient wich broke the gradient computation

AnetLib/models/networks_tensorflow.py
@@ -1811,7 +1811,6 @@ def create_revgan_model(inputs, targets, controls, channel_masks, ngf=64, ndf=64
             backward_outputs_lr = generate_revgan_backward_punet(targets, controls, lr_nc, ngf, bayesian_dropout=bayesian_dropout,
                                             dropout_prob=dropout_prob, output_num=1,
                                             use_resize_conv=use_resize_conv, revnet=revnet, use_skip_connections=use_skip_connections)
-            backward_outputs_lr = tf.stop_gradient(backward_outputs_lr)
 
 
             backward_outputs_fake_lr = generate_revgan_backward_punet(outputs, controls, lr_nc, ngf,
@@ -1975,11 +1974,7 @@ def create_revgan_model(inputs, targets, controls, channel_masks, ngf=64, ndf=64
                 gen_loss = gen_loss_L1
 
         if not no_lsgan:
-            if output_uncertainty:
-                # "VALID" mode in convolution only ever drops the right-most columns (or bottom-most rows).
-                bw_gen_loss_GAN = None
-            else:
-                bw_gen_loss_GAN = tf.reduce_mean(tf.square(lr_predict_fake - 1))
+            bw_gen_loss_GAN = tf.reduce_mean(tf.square(lr_predict_fake - 1))
         else:
             bw_gen_loss_GAN = tf.reduce_mean(-tf.log(lr_predict_fake + EPS))
 

AnetLib/models/revnet.py
@@ -158,7 +158,10 @@ class ReversibleNet():
         """
         # First , reverse the layer to r e t r i e v e inputs
         y1, y2 = outputs[0], outputs[1]
-        F_weights, G_weights = tf.split(layer_weights, num_or_size_splits=2, axis=0)
+        # F_weights , G_weights = tf.split(layer_weights, num_or_size_splits=2, axis=1)
+        split_index = int(len(layer_weights) / 2)
+        F_weights = layer_weights[0:split_index]
+        G_weights = layer_weights[split_index:]
 
         with tf.variable_scope("rev_block"):
             z1_stop = tf.stop_gradient(y1)
@@ -256,7 +259,9 @@ class ReversibleNet():
         """
         # First , reverse the layer to r e t r i e v e inputs
         x1, x2 = outputs[0], outputs[1]
-        F_weights, G_weights = tf.split(layer_weights, num_or_size_splits=2, axis=0)
+        split_index = int(len(layer_weights) / 2)
+        F_weights = layer_weights[0:split_index]
+        G_weights = layer_weights[split_index:]
 
         with tf.variable_scope("rev_block"):
             x2_stop = tf.stop_gradient(x2)
@@ -288,8 +293,9 @@ class ReversibleNet():
 
 
 def rev_block(in_1, in_2, weights, reverse):
-    weights_f, weights_g = tf.split(weights, num_or_size_splits=2, axis=0)
-
+    split_index = int(len(weights) / 2)
+    weights_f = weights[0:split_index]
+    weights_g = weights[split_index:]
     with tf.variable_scope("rev_block"):
         if reverse:
             # x2 = y2 - NN2(y1)