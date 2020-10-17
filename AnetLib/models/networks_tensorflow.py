fixed error in revnet where not all gradients were returned built test_model to test gradient computation

AnetLib/models/networks_tensorflow.py
@@ -1464,43 +1464,107 @@ def create_test_model(inputs, targets, controls, channel_masks, ngf=64, ndf=64, 
             inputs = preprocess(inputs, mode='mean_std')
         inputs = inputs * channel_masks
 
-    in_1 = inputs
-    in_2 = inputs
+        in_1 = inputs
+        in_2 = inputs
 
-    revnet = ReversibleNet(20, 2)
+        revnet = ReversibleNet(51, 2)
 
-    out_11, out_22 = revnet.forward_pass((in_1, in_2))
-    out_1, out_2 = revnet.backward_pass((out_11, out_22))
+        outp_11,outp_22 = revnet.backward_pass_activations((in_1, in_2))
+        outpputs = tf.concat((outp_11, outp_22), axis=3)
 
-    output11, output22 = out_11[:, :, :, 0:1], out_11[:, :, :, 1:2]
-    output1, output2 = out_1[:,:,:,0:1],out_1[:,:,:,1:2]
-    input1, input2 = in_1[:,:,:,0:1],in_1[:,:,:,1:2]
+        out_11, out_22 = revnet.backward_pass((in_1, in_2))
+        out_1, out_2 = revnet.forward_pass((out_11, out_22))
 
-    dp_out1 = deprocess(output1)
-    dp_out2 = deprocess(output2)
-    dp_in1 = deprocess(input1)
-    dp_in2 = deprocess(input2)
-    dp_targets = deprocess(targets)
+        outputs = tf.concat((out_11, out_22), axis=3)
 
-    global_step = tf.train.get_or_create_global_step()
-    incr_global_step = tf.assign(global_step, global_step + 1)
+        output11, output22 = out_11[:, :, :, 0:1], out_11[:, :, :, 1:2]
+        output1, output2 = out_1[:,:,:,0:1],out_1[:,:,:,1:2]
+        input1, input2 = in_1[:,:,:,0:1],in_1[:,:,:,1:2]
+
+
+    with tf.name_scope("generator_train_p"):
+        dependencies = []
+        with tf.control_dependencies(dependencies):
+            # compute gradients for decoder part
+            lossp = tf.reduce_mean(tf.square(1 - outpputs))
+
+            dec_vars = [var for var in tf.trainable_variables() if var.name.startswith("generator/")]
+            dec_grads = tf.gradients(lossp,dec_vars)
+            gen_grads_and_vars = np.array(list(zip(dec_grads, dec_vars)))
+
+            gen_vars = gen_grads_and_vars[:, 1]
+            gen_grads = gen_grads_and_vars[:, 0]
+
+            print(gen_vars)
+
+            # apply gradients to graph
+            with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
+                gen_optim = tf.train.AdamOptimizer(lr, beta1)
+                gen_trainp = gen_optim.apply_gradients(gen_grads_and_vars)
+
+
+    with tf.name_scope("generator_train"):
+        dependencies = []
+        with tf.control_dependencies(dependencies):
+            # compute gradients for decoder part
+            loss = tf.reduce_mean(tf.square(1 - outputs))
+
+            rev_out_1_var = out_11
+            rev_out_2_var = out_22
+
+            dec_grads = tf.gradients(loss, [rev_out_1_var, rev_out_2_var], stop_gradients=[rev_out_1_var, rev_out_2_var])
+            rev_out_1_grad = dec_grads[0]
+            rev_out_2_grad = dec_grads[1]
+
+            # manual gradients for revnet
+            (dy1, dy2), rev_grads_and_vars = revnet.compute_revnet_gradients_of_backward_pass(rev_out_1_var,
+                                                                                             rev_out_2_var,
+                                                                                             rev_out_1_grad,
+                                                                                             rev_out_2_grad)
+
+
+
+            rev_grads_and_vars = np.array(rev_grads_and_vars)
+            rev_vars = rev_grads_and_vars[:,1]
+            rev_grads = rev_grads_and_vars[:, 0]
+            print(rev_vars)
+
+            gen_grads_and_vars = rev_grads_and_vars.tolist()
+
+            # apply gradients to graph
+            with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
+                gen_optim = tf.train.AdamOptimizer(lr, beta1)
+                gen_train = gen_optim.apply_gradients(gen_grads_and_vars)
+
+    with tf.name_scope("generator_grad_diff"):
+        grad_diff = rev_grads[62]
+        grad_diff = tf.transpose(grad_diff, [3,0,1,2])
+        grad_diff = grad_diff[0:1,:,:,0:1]
+
+        grad_diff1 = gen_grads[62]
+        grad_diff1 = tf.transpose(grad_diff1, [3, 0, 1, 2])
+        grad_diff1 = grad_diff1[0:1, :, :, 0:1]
+
+    with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
+        global_step = tf.train.get_or_create_global_step()
+        incr_global_step = tf.assign(global_step, global_step + 1)
 
     return Model(
         type='UNET',
-        predict_real=output11,
-        predict_fake=output22,
+        predict_real=out_11[:, :, :, 0:1],
+        predict_fake=outp_11[:, :, :, 0:1],
         inputs=input2,
         lr_inputs=scaled_lr_inputs,
-        lr_predict_real=None,
-        lr_predict_fake=None,
+        lr_predict_real=grad_diff,
+        lr_predict_fake=grad_diff1,
         squirrel_error_map=None,
         squirrel_discrim_loss=None,
         squirrel_discrim_grads_and_vars=None,
         discrim_loss=None,
         discrim_grads_and_vars=None,
         gen_loss_GAN=None,
-        gen_loss=None,
-        gen_loss_L1=None,
+        gen_loss=loss,
+        gen_loss_L1=lossp,
         gen_loss_L2=None,
         gen_loss_SSIM=None,
         gen_loss_squirrel=None,
@@ -1509,11 +1573,11 @@ def create_test_model(inputs, targets, controls, channel_masks, ngf=64, ndf=64, 
                   'gen_loss_L1': None, 'gen_loss_L2': None,
                   'gen_loss_SSIM': None, 'gen_loss_squirrel': None,
                   'squirrel_discrim_loss': None},
-        outputs=output2,
-        targets=output22,
+        outputs=output2[:, :, :, 0:1],
+        targets=output22[:, :, :, 0:1],
         uncertainty=None,
         squirrel_discrim_train=None,
-        train=tf.group(incr_global_step, dp_out1, dp_out2),
+        train=tf.group(incr_global_step, gen_train, gen_trainp),
     )
 
 def create_pix2pix_model(inputs, targets, controls, channel_masks, ngf=64, ndf=64, discriminator_layer_num=3, output_uncertainty=False,
@@ -2028,6 +2092,7 @@ def create_revgan_model(inputs, targets, controls, channel_masks, ngf=64, ndf=64
                                                                                                  rev_out_2_var,
                                                                                                  rev_out_1_grad,
                                                                                                  rev_out_2_grad)
+                print(rev_grads_and_vars)
                 rev_grads_and_vars = np.array(rev_grads_and_vars)
             else:
                 (dy1, dy2) = (rev_out_1_grad, rev_out_2_grad)

AnetLib/models/revnet.py
@@ -12,6 +12,71 @@ class ReversibleNet():
         for i in range(len(self.weight_list)):
             self.weight_ta = self.weight_ta.write(i, self.weight_list[i])
 
+    def forward_pass_activations(self, inputs):
+        """
+        Generates forward pass of the revnet
+        Args:
+            inputs: tuple of two same sized input tensors
+        Returns:
+            layer_rev_out: tuple of the two ouput tensors of the last network layer
+        """
+        print("revnet")
+        print(inputs[0].shape)
+        print(inputs[1].shape)
+
+        for i in range(self.num_blocks):
+            layer_weights = []
+            for j in range(self.weights_per_layer):
+                layer_weights.append(self.weight_ta.read(i * self.weights_per_layer + j))
+
+            in_1, in_2 = inputs
+            out_1, out_2 = rev_block(in_1, in_2, layer_weights, reverse=False)
+            out_1.set_shape(in_1.get_shape())
+            out_2.set_shape(in_1.get_shape())
+            inputs = (out_1, out_2)
+
+        return inputs
+
+    def backward_pass_activations(self, inputs):
+        """
+        Generates forward pass of the revnet
+        Args:
+            inputs: tuple of two same sized input tensors
+        Returns:
+            layer_rev_out: tuple of the two ouput tensors of the last network layer
+        """
+        print("revnet")
+        print(inputs[0].shape)
+        print(inputs[1].shape)
+
+        for i in range(self.num_blocks - 1, -1, -1):
+            layer_weights = []
+            for j in range(self.weights_per_layer):
+                layer_weights.append(self.weight_ta.read(i * self.weights_per_layer + j))
+
+            in_1, in_2 = inputs
+            out_1, out_2 = rev_block(in_1, in_2, layer_weights, reverse=True)
+            out_1.set_shape(in_1.get_shape())
+            out_2.set_shape(in_1.get_shape())
+            inputs = (out_1, out_2)
+
+        return inputs
+
+    def def_rev_block_weights(self, channels):
+        """
+        Defines weights for the revnet.
+        Args:
+            channels: channel number of the input tensors
+        Returns:
+            weight_list: list of all weights for the revnet, sorted alphabetically by variable-scope/name
+        """
+        weight_list = []
+        for i in range(self.num_blocks):
+            with tf.variable_scope("rev_core_%03d" % (i + 1)):
+                weight_list = rev_block_weights(channels, weight_list)
+        return weight_list
+
+
     def forward_pass(self, inputs):
         """
         Generates forward pass of the revnet
@@ -74,21 +139,6 @@ class ReversibleNet():
         return layer_rev_out
 
 
-    def def_rev_block_weights(self, channels):
-        """
-        Defines weights for the revnet.
-        Args:
-            channels: channel number of the input tensors
-        Returns:
-            weight_list: list of all weights for the revnet, sorted alphabetically by variable-scope/name
-        """
-        weight_list = []
-        for i in range(self.num_blocks):
-            with tf.variable_scope("rev_core_%03d" % (i + 1)):
-                weight_list = rev_block_weights(channels, weight_list)
-        return weight_list
-
-
     def compute_revnet_gradients_of_forward_pass(self, y1, y2, dy1, dy2):
         """
         Computes gradients.
@@ -138,7 +188,7 @@ class ReversibleNet():
                                                                       output_grads, weights, weights_grads],
                                                                      parallel_iterations=1, back_prop=False)
 
-            for i in range(self.num_blocks):
+            for i in range(self.num_blocks * self.weights_per_layer):
                 grads_list.append(weights_grads.read(index=i))
 
             return input_grads, list(zip(grads_list, self.weight_list))
@@ -234,12 +284,12 @@ class ReversibleNet():
                 input_grads[0].set_shape(output_grads[0].get_shape())
                 return [layer_index + 1, inputs, input_grads, weights, weights_grads]
 
-            _, inputs, input_grads, _, weights_grads = tf.while_loop(lambda i, *_: i < self.num_blocks - 1, loop_body,
+            _, inputs, input_grads, _, weights_grads = tf.while_loop(lambda i, *_: i <= self.num_blocks - 1, loop_body,
                                                                      [tf.constant(0), outputs,
                                                                       output_grads, weights, weights_grads],
                                                                      parallel_iterations=1, back_prop=False)
 
-            for i in range(self.num_blocks):
+            for i in range(self.num_blocks * self.weights_per_layer):
                 grads_list.append(weights_grads.read(index=i))
 
             return input_grads, list(zip(grads_list, self.weight_list))
@@ -324,7 +374,6 @@ def res_block(in_1, weights):
             out_1 = rev_conv3x3(in_1, tf.squeeze(weights_1[2]))
             out_1 = rev_batchnorm(out_1, weights_1[0:2])
             out_1 = rev_lrelu(out_1, 0.2)
-            #out_1 = tf.tanh(out_1)
         return out_1
 
 def rev_conv3x3(batch_input, weight):